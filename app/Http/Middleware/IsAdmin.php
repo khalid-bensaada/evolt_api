3774a1ed45a6478e30c47e4590a07faced59a10a
khalid-bensaada <kbensaada953@gmail.com>
2026-03-11 11:08:17 +0000

    added a middleware for admin

---
+++ app/Http/Middleware/IsAdmin.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class IsAdmin
+{
+    public function handle(Request $request, Closure $next)
+    {
+
+        if (!$request->user() || $request->user()->role !== 'admin') {
+            return response()->json([
+                'message' => 'Access denied. Admins only.'
+            ], 403);
+        }
+
+        return $next($request);
+    }
+}
